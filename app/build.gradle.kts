0684d3bd3a498ed9e33708827d96fba18d5d4279
ph0d <ph0d@ed800g.nexa.work>
2025-12-25 23:33:16 -0600

    Configure Java 21 toolchain
    - Pin JavaCompile tasks to JDK 21 via Gradle toolchains
    
    - Set Kotlin JVM toolchain to 21 for Android module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,10 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
+kotlin {
+    jvmToolchain(21)
+}
+
 android {
     namespace = "com.smstest.app"
     compileSdk = 35
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,22 @@
+import org.gradle.api.tasks.compile.JavaCompile
+import org.gradle.jvm.toolchain.JavaLanguageVersion
+import org.gradle.jvm.toolchain.JavaToolchainService
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.8.0" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
+}
+
+val javaToolchains = extensions.getByType<JavaToolchainService>()
+
+subprojects {
+    tasks.withType<JavaCompile>().configureEach {
+        javaCompiler.set(
+            javaToolchains.compilerFor {
+                languageVersion.set(JavaLanguageVersion.of(21))
+            }
+        )
+    }
 }
